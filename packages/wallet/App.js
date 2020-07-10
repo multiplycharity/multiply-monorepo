75aeb6a33b06b9b0688d90fe6841284154e6c17e
Amir <amiromayer@gmail.com>
2020-07-10 16:06:42 +0500

    Add monorepo

---
+++ packages/wallet/App.js
@@ -0,0 +1,28 @@
+import * as React from 'react'
+import { Text, View, Splash } from 'react-native'
+
+import { NotifierWrapper } from 'react-native-notifier'
+
+import { Provider } from 'react-redux'
+import { store, persistor } from './src/redux/store'
+import { PersistGate } from 'redux-persist/integration/react'
+
+import RootNavigator from './src/navigation/RootNavigator'
+
+if (__DEV__) {
+  import('./src/config/reactotron').then(() => {
+    // console.log('Reactotron Configured')
+  })
+}
+
+export default function App () {
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <NotifierWrapper>
+          <RootNavigator />
+        </NotifierWrapper>
+      </PersistGate>
+    </Provider>
+  )
+}
---
+++ packages/wallet/README.md
@@ -0,0 +1,29 @@
+### Clone repo
+
+```js
+git clone https://github.com/amiromayer/avacash.git
+```
+
+### Install dependencies
+
+```js
+yarn install
+```
+
+### Configure env vars
+
+Copy .env.sample content into .env file and feel free to customize with your params, otherwise just use provided
+
+### Start ios app and server
+
+This step assumes you already have XCode and iOS simulator installed on your Mac
+
+```js
+yarn ios
+```
+
+## Try app
+
+You can also try app in Expo Client App
+
+https://expo.io/@amiromayer/avacashwallet
